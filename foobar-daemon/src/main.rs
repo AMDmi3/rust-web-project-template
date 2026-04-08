3d99f722e3a9557d5629356f2f3eeba7f1b7efca
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-04-08 17:36:26 +0300

    daemon: Remove unused feature

--- foobar-daemon/src/main.rs
+++ foobar-daemon/src/main.rs
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: Copyright 2025 Dmitry Marakasov <amdmi3@amdmi3.ru>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#![feature(coverage_attribute)]
 #![feature(try_blocks)]
 
 mod config;
