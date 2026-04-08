1b75a6890af0adb534c8f1e0cec3ad2e85ffd0e4
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-04-08 17:15:48 +0300

    web: Update middleware to use route information

--- foobar-web/src/lib.rs
+++ foobar-web/src/lib.rs
@@ -16,7 +16,7 @@ use std::time::Instant;
 use axum::{
     Router,
     body::HttpBody,
-    extract::{MatchedPath, Request},
+    extract::Request,
     middleware::{self, Next},
     response::IntoResponse,
 };
@@ -26,42 +26,24 @@ use sqlx::PgPool;
 use tracing::info;
 
 //use crate::config::AppConfig;
-use crate::endpoints::Endpoint;
+use crate::endpoints::{Endpoint, SelfEndpoint};
 use crate::state::AppState;
 use crate::static_files::STATIC_FILES;
 
-async fn track_metrics(matched_path: MatchedPath, req: Request, next: Next) -> impl IntoResponse {
+async fn track_metrics(endpoint: SelfEndpoint, request: Request, next: Next) -> impl IntoResponse {
     let start = Instant::now();
-
-    let path_for_metrics = {
-        // normalize some paths which lead to the same endpoints; XXX this will hopefully be gone
-        // someday when endpoints are redesigned (e.g. /projects/{bound}/ → /projects/?start=)
-        let mut path = matched_path
-            .as_str()
-            .trim_end_matches("{bound}/")
-            .trim_end_matches("/{sorting}");
-        if path.starts_with("/graph/total/") {
-            path = "/graph/total/..."
-        }
-        if path.starts_with("/graph/repo/") {
-            path = "/graph/repo/..."
-        }
-
-        path.to_owned()
-    };
-
-    let response = next.run(req).await;
-
+    let response = next.run(request).await;
     let latency = start.elapsed().as_secs_f64();
+
+    let route_name = endpoint.path();
     let status = response.status().as_u16().to_string();
 
-    counter!("foobar_web_http_requests_total", "path" => path_for_metrics.clone(), "status" => status)
+    counter!("foobar_web_http_requests_total", "route" => route_name, "status" => status)
         .increment(1);
-    histogram!("foobar_web_http_requests_duration_seconds", "path" => path_for_metrics.clone())
-        .record(latency);
+    histogram!("foobar_web_http_requests_duration_seconds", "route" => route_name).record(latency);
 
     if let Some(body_size) = response.body().size_hint().exact() {
-        histogram!("foobar_web_http_response_size_bytes", "path" => path_for_metrics)
+        histogram!("foobar_web_http_response_size_bytes", "route" => route_name)
             .record(body_size as f64);
     }
 
@@ -76,7 +58,8 @@ pub async fn create_app(pool: PgPool) -> anyhow::Result<Router> {
     let _ = &*STATIC_FILES;
 
     info!("initializing routes");
-    Ok(Endpoint::add_to_router(Router::new())
-        .route_layer(middleware::from_fn(track_metrics))
-        .with_state(state))
+    Ok(
+        Endpoint::to_router_with(|router| router.layer(middleware::from_fn(track_metrics)))
+            .with_state(state),
+    )
 }
