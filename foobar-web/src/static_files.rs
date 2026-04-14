8bab0466b4b8d6ed7a85df3906159589c18212e2
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-04-14 20:12:22 +0300

    web: Update axum-myruotes and use stricter path parameter API

--- foobar-web/src/static_files.rs
+++ foobar-web/src/static_files.rs
@@ -119,7 +119,8 @@ pub fn url_for_static(file_name: &str) -> anyhow::Result<String> {
 
     Ok(crate::routes::Route::StaticFile
         .url_for()
-        .param("file_name", &file.hashed_name)
+        .path_param("file_name", &file.hashed_name)
+        .expect("file_name parameter should exist for StaticFile route")
         .build()?)
 }
 
@@ -127,6 +128,7 @@ pub fn url_for_static(file_name: &str) -> anyhow::Result<String> {
 pub fn url_for_unversioned_static(file_name: &str) -> anyhow::Result<String> {
     Ok(crate::routes::Route::StaticFile
         .url_for()
-        .param("file_name", file_name)
+        .path_param("file_name", file_name)
+        .expect("file_name parameter should exist for StaticFile route")
         .build()?)
 }
