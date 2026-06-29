5583fd19c8429728f1d196e08d4e3dbc7946dcfb
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-06-29 19:04:00 +0300

    *: Update remaining copyright years

--- foobar-common/src/lib.rs
+++ foobar-common/src/lib.rs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2024 Dmitry Marakasov <amdmi3@amdmi3.ru>
+// SPDX-FileCopyrightText: Copyright 2026 Dmitry Marakasov <amdmi3@amdmi3.ru>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 pub mod db;
